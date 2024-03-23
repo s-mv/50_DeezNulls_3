3feb458fc512ea1fb239e44f5da9ed52fa7d7dff
Kushl <kushlalve007@gmail.com>
2024-03-23 22:29:59 +0530

    added navbar and recruiter form

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import "./navbar.css";
+import logo_light from "../../assets/logo-black.png";
+import logo_dark from "../../assets/logo-white.png";
+import search_light from "../../assets/search-w.png";
+import search_dark from "../../assets/search-b.png";
+import toggle_light from "../../assets/night.png";
+import toggle_dark from "../../assets/day.png";
+import Recruiter from '../pages/recruiter';
+
+const Navbar = (props) => {
+    const toggleTheme = () => {
+        props.theme == "light" ? props.setTheme("dark") : props.setTheme("light");
+    }
+
+    return (
+        <div className="container">
+            <div className={`navbar ${props.theme}`}>
+                <h1><a href="/">TalentHub</a></h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li><button>Login/SignUp</button></li>
+                </ul>
+
+                <div className={`search-box ${props.theme}`}>
+                    <input type="text" placeholder='Search...' />
+                    <img src={props.theme == "light" ? search_light : search_dark} alt="" />
+                </div>
+
+                <img src={props.theme == "light" ? toggle_light : toggle_dark} alt="" className='toggle-icon' onClick={toggleTheme} />
+            </div>
+            <div className='body'>
+                <Recruiter />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/components/pages/recruiter.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "./recruiter.css";
+
+function Recruiter() {
+
+    const [keywords, setKeywords] = useState([""]);
+    const [resumes, setResumes] = useState([]);
+
+    const handleAddKeyword = () => {
+        setKeywords([...keywords, ""]);
+    };
+
+    const handleResumeUpload = (event) => {
+        const files = Array.from(event.target.files);
+        setResumes(prevResumes => [...prevResumes, ...files]);
+    };
+
+
+    const handleKeywordChange = (index, event) => {
+        const newKeywords = [...keywords];
+        newKeywords[index] = event.target.value;
+        setKeywords(newKeywords);
+    };
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        // Handle form submission
+    }
+
+    return (
+        <div className='dashboard'>
+            <div className='left'>
+                <h2>Submit applied resumes</h2>
+                <form>
+                    <label htmlFor="resumeUpload">Upload Resume</label>
+                    <input
+                        type="file"
+                        id="resumeUpload"
+                        multiple
+                        onChange={handleResumeUpload}
+                        className='resume-input'
+                    />
+                    {resumes.length > 0 && (
+                        <div>
+                            <h3>Uploaded Resumes:</h3>
+                            <ul>
+                                {resumes.map((file, index) => (
+                                    <li key={index}>{file.name}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </form>
+            </div>
+            <div className='right'>
+                <h2>Enter keywords for filtering resumes</h2>
+                <form>
+                    {keywords.map((keyword, index) => (
+                        <div key={index}>
+                            <label>Label {index + 1}</label>
+                            <input
+                                type="text"
+                                className='keyword-input'
+                                value={keyword}
+                                onChange={(event) => handleKeywordChange(index, event)}
+                            />
+                        </div>
+                    ))}
+                    <button type="button" onClick={handleAddKeyword} className="add-btn">+ Add</button>
+                    {/* <button className='submit-btn' type="submit">Submit</button> */}
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Recruiter;
